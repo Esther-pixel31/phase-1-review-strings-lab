c09a3ecf4bf2613519b947ea8e5253aa42fc2143
EstherMutua <esthermutua16@gmail.com>
2025-03-04 21:21:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!!!`;
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,4)}!`;
 
